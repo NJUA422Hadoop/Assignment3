57d760c346ce2da94f7231d9060bdb8d4340b083
WY <31894719+WaterYe@users.noreply.github.com>
2018-04-24 13:56:11 +0800

    map v0.1
    map v0.1

--- src/main/java/mapper/InvertedIndexMapper.java
+++ src/main/java/mapper/InvertedIndexMapper.java
@@ -7,6 +7,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+/**
+ * 输入: key为Object类型, 代表行偏移量, value为Text类型, 代表行内容
+ * 输出: key为Text类型, 格式为word@fileName, value为IntWritable类型, 代表词在文中出现一次
+ * @author WaterYe(王尧 151220112)
+ */
+
 public class InvertedIndexMapper extends Mapper<Object, Text, Text, IntWritable> {
     public final static Class outputKeyClass = Text.class;
     public final static Class outputValueClass = IntWritable.class;
@@ -15,9 +21,11 @@ public class InvertedIndexMapper extends Mapper<Object, Text, Text, IntWritable>
     private Text word = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        FileSplit fileSplit = (FileSplit)context.getInputSplit();
+        String fileName = fileSplit.getPath().getName();
         StringTokenizer itr = new StringTokenizer(value.toString());
         while(itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
+            word.set(itr.nextToken() + "@" + fileName);
             context.write(word, one);
         }
     }
