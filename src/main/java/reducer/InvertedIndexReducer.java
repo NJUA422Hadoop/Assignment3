1fb2dcc383188806808f76b78960085896dcaca3
qianlanwyd <646842131@qq.com>
2018-04-26 20:47:28 +0800

    reduce
    small fix

--- src/main/java/reducer/InvertedIndexReducer.java
+++ src/main/java/reducer/InvertedIndexReducer.java
@@ -12,6 +12,7 @@ public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
   public final static Class outputValueClass = Text.class;
   private String term = new String();//临时存储word#filename中的word
   private String last = " ";//临时存储上一个word
+  private int if_first=1;
   private int countItem;//统计word出现次数
   private int countDoc;//统计word出现文件数
   private StringBuilder out = new StringBuilder();//临时存储输出的value部分
@@ -20,7 +21,7 @@ public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
   public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       term = key.toString().split(intervals)[0];//获取word
       if (!term.equals(last)) {//此次word与上次不一样，则将上次进行处理并输出
-          if (!last.equals(" ")) {//避免第一次比较时出错
+        if (if_first==0){
               out.setLength(out.length() - 1);//删除value部分最后的;符号
               f = (float) countItem / countDoc;//计算平均出现次数
               context.write(new Text(last), new Text(String.format("%.2f,%s", f, out.toString())));//value部分拼接后输出
@@ -29,6 +30,7 @@ public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
               out = new StringBuilder();
           }
           last = term;//更新word，为下一次做准备
+          if_first=0;
       }
       int sum = 0;//累加相同word和filename中出现次数
       for (IntWritable val : values) {
