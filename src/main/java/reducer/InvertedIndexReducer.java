b579e8b49770ef43c50dbaa746c8fbfe4d9aacc9
qianlanwyd <646842131@qq.com>
2018-04-28 23:36:51 +0800

    type fix
    stringbuilder to string

--- src/main/java/reducer/InvertedIndexReducer.java
+++ src/main/java/reducer/InvertedIndexReducer.java
@@ -15,19 +15,19 @@ public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
   private int if_first=1;
   private int countItem;
   private int countDoc;
-  private StringBuilder out = new StringBuilder();
+  private String out = new String();
   private float f;
   public static final String intervals = "[@#]";
   public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       term = key.toString().split(intervals)[0];
       if (!term.equals(last)) {
         if (if_first==0){
-              out.setLength(out.length() - 1);
+              out=out.substring(0, out.length()-1);
               f = (float) countItem / countDoc;
-              context.write(new Text(last), new Text(String.format("%.2f,%s", f, out.toString())));
+              context.write(new Text(last), new Text(String.format("%.2f,%s", f, out)));
               countItem = 0;
               countDoc = 0;
-              out = new StringBuilder();
+              out = new String();
           }
           last = term;
           if_first=0;
@@ -36,13 +36,13 @@ public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
       for (IntWritable val : values) {
           sum += val.get();
       }
-      out.append(key.toString().split(intervals)[1] + ":" + sum + ";");
+      out=out+key.toString().split(intervals)[1] + ":" + sum + ";";
       countItem += sum;
       countDoc += 1;
   }
   public void cleanup(Context context) throws IOException, InterruptedException {
-      out.setLength(out.length() - 1);
+      out=out.substring(0, out.length()-1);
       f = (float) countItem / countDoc;
-      context.write(new Text(last), new Text(String.format("%.2f,%s", f, out.toString())));
+      context.write(new Text(last), new Text(String.format("%.2f,%s", f, out)));
   }
 }
