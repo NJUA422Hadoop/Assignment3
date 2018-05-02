6eda8e298b3ac487af02ccd003b108a63aa15e83
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-05-02 20:29:54 +0800

    InvertedIndexAssignment
    - import

--- src/main/java/InvertedIndexAssignment.java
+++ src/main/java/InvertedIndexAssignment.java
@@ -11,10 +11,7 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-import org.apache.hadoop.mapred.lib.HashPartitioner;
 /**
  * InvertedIndexAssignment
  * 依赖实现：
