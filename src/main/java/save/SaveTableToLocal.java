932c651f84bb5bcbb87317c6f108e78a73698df1
WY <31894719+WaterYe@users.noreply.github.com>
2018-05-14 17:53:32 +0800

    save HBase table to local file

---
+++ src/main/java/save/SaveTableToLocal.java
@@ -0,0 +1,35 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
+import java.io.*;
+
+/**
+ * Created by WaterYe on 18-5-14.
+ */
+public class SaveTableToFile {
+    public static void main(String []args) throws IOException {
+        File file = new File("table.txt");
+        if(!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fw = new FileWriter(file, true);
+        PrintWriter pw = new PrintWriter(file);
+        Configuration configuration = HBaseConfiguration.create();
+        HTable table = new HTable(configuration, "Wuxia");
+        ResultScanner rs = table.getScanner(new Scan());
+        StringBuilder sb = new StringBuilder();
+        for(Result r : rs) {
+            String word = new String(r.getRow());
+            String value = new String(r.getValue("count".getBytes(), null));
+            sb.append(word + "\t" + value + "\n");
+        }
+        pw.print(sb.toString());
+        pw.flush();
+        pw.close();
+        fw.close();
+    }
+}
