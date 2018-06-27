cfc0f500752d599a057bcb76bbd62df67622eaa6
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-06-28 02:10:43 +0800

    Mission2
    debug，这次行为应该正确了。

--- src/main/java/mission2/mapper/TheMapper.java
+++ src/main/java/mission2/mapper/TheMapper.java
@@ -1,6 +1,8 @@
 package mission2.mapper;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -9,25 +11,21 @@ import org.apache.hadoop.mapreduce.Mapper;
 import mission2.tools.TheKey;
 
 public class TheMapper extends Mapper<Object, Text, TheKey, IntWritable> {
-  private final static IntWritable one = new IntWritable(1);
+  private final Set<Text> names = new HashSet<>();
+  private final IntWritable one = new IntWritable(1);
 
   @Override
   protected void map(Object key, Text value, Mapper<Object, Text, TheKey, IntWritable>.Context context)
     throws IOException, InterruptedException {
-    String[] names = value.toString().split(" ");
+    names.clear();
 
-    for (String a : names) {
-      for (String b : names) {
-        if (a == b) {
-          continue;
-        }
+    for (String name : value.toString().split(" ")) {
+      names.add(new Text(name));
+    }
 
-        context.write(
-          new TheKey(
-            new Text(a),
-            new Text(b)
-          ), one
-        );
+    for (Text a : names) {
+      for (Text b : names) {
+        context.write(new TheKey(a, b), one);
       }
     }
   }
