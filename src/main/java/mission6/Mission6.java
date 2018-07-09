25c29f9bb2feb4a6765d8f8cd423b42a4463fb63
qianlanwyd <646842131@qq.com>
2018-07-09 13:30:37 +0800

    jianjie
    1

--- src/main/java/mission6/Mission6.java
+++ src/main/java/mission6/Mission6.java
@@ -7,8 +7,10 @@ import tools.BaseMission;
 
 /**
  * 第六个任务
- * 做这个任务的人写一下描述：
- *  XXX
+ * 分析结果整理
+    （1）PageRank值进行排序
+    （2）将属于同一个标签的人物输出到一起
+ *  wyd
  */
 
 public class Mission6 extends BaseMission {
